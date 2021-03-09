e354ca0a8a5734df040710b200cc46ea53338e33
majsterkovic <mariusz8h@o2.pl>
2021-03-09 14:00:16 +0100

    test

--- contact.php
+++ contact.php
@@ -1,30 +1,23 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: majsterkovic@github.io';
+    $to = 'majsterkovic@gmail.com';
+    $email_subject = 'New Contact Form Submission!';
+    $body = "Name: $name\nE-mail: $email\n\nThe message is below:\n$message";;
+?>
 
-<?php session_start();
-if(isset($_POST['submit'])) {
-$youremail = 'yourname@domain.com';
-$fromsubject = 'Contact Form';
-$name = $_POST['name'];
-$mail = $_POST['email'];
-$subject = $_POST['subject']; 
-$message = $_POST['message']; 
-$to = $youremail; 
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type:text/html; charset=UTF-8' . "\r\n";
-$headers .= "From: ".$_POST['name']."<".$_POST['Email'].">\r\n"; 
-$headers .= "Reply-To: ".$_POST["email"]."\r\n";
-$mailsubject = 'Messsage recived for'.$fromsubject.' Contact Page';
-$body = $fromsubject.'
-	
-	The person that contacted you is  '.$name.'
-	 E-mail: '.$mail.'
-	 Subject: '.$subject.'
-	
-	 Message: 
-	 '.$message.'	
-	|---------END MESSAGE----------|'; 
-echo "Thank you fo your feedback. I will contact you shortly if needed.<br/>Go to <a href='/index.php'>Home Page</a>"; 
-								mail($to, $subject, $body,$headers);
- } else { 
-echo "You must write a message. </br> Please go to <a href='/index.html'>Home Page</a>"; 
-}
-?> 
+<?php
+    if (isset($_POST['submitbtn']))
+    {
+        if (mail($to, $email_subject, $body, $from))
+        {
+            echo "<font color=\"green\"><p>Your message has been sent!</p></font>";
+        }
+        else
+        {
+        echo "<font color=\"red\"><p>Your message sending has failed! Please manually email (your email)!</p></font>";
+        }
+    }
+?>
